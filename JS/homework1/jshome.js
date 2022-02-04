486c5577e2459694b950a32498efa4e9c6fb9c09
mac <mpertak91@gmail.com>
2022-02-04 12:53:14 +0200

    js

---
+++ JS/homework1/jshome.js
@@ -0,0 +1,98 @@
+ //1Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+ let txt1 = 'hello ';
+ let txt2 = 'owu';
+ let txt3 ='com';
+ let txt4 ='ua';
+ let number1= 1 ;
+ let number2= 10 ;
+ let number3= -999 ;
+ let number4= 123 ;
+let number5= 3.14 ;
+let number6= 2.7 ;
+ let number7= 16;
+  let yes= 'true';
+  let no = 'false';
+ //2Вивести кожну змінну за допомогою: console.log , alert, document.write
+ alert(txt1);
+ alert(txt2);
+ alert(txt3);
+ alert(txt4);
+ console.log (number1);
+ console.log (number2);
+ console.log (number3 );
+ console.log (number4);
+ console.log (number5);
+ console.log (number6);
+ console.log (number7);
+ console.log(yes);
+ console.log(no);
+ //3- Переприсвоїти кожній змінній з завдання значення на довільне.
+ txt1 ='hi';
+ txt2 ='whats';
+ txt3 ='yuor';
+ txt4='name?';
+ number1=23;
+ number2=43;
+number3=1;
+ number4=5;
+ number5=123;
+ number6=46;
+ number7=65;
+ yes='im';
+ no='man';
+ //4Вивести кожну змінну за допомогою: console.log , alert, document.write
+ document.write('<div>' + txt1 + '</div>');
+document.write('<div>' + txt2 + '</div>');
+ document.write('<div>' + txt3 + '</div>');
+ document.write('<div>' + txt4 + '</div>');
+ console.log(number1);
+ console.log(number2);
+ console.log(number3);
+ console.log(number4);
+ console.log(number5);
+ console.log(number6);
+console.log(number7);
+ alert(yes);
+ alert(no);
+
+ //5- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+let fn ='Шевченко';
+ let mdn='Тарас';
+ let lt='Григорович';
+let person = [fn,mdn,lt];
+//6 За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+let im =prompt('Marian');
+ let id =prompt('Igorovich');
+ let age =prompt( '30');
+ // 7 Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+ let str =prompt('Vitayu')
+let im =prompt('Marian');
+ let id =prompt('Igorovich');
+ let str1=prompt('Tobi')
+ let age =prompt( '30');
+ let str2=prompt('Rokiv')
+ console.log( (`${str} ${im}  ${id} ${str1} ${age} ${str2} ` ))
+
+ // За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+ let a = 100;//number
+ let b = '100';//string
+ let c = true;//boolean
+ console.log (typeof 100);
+ console.log(typeof '100');
+console.log(typeof  true)
+
+// Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+    // В однакових виразах не використовувати однакові оператори!!!
+ console.log (5 < 6 );
+ console.log(5 > 6) ;
+ console.log(5 >= 6) ;
+ console.log(5 == 6 );
+ console.log (10 ===10);
+ console.log(10 <=   10);
+ console.log(10 !=  10);
+ console.log(10  !== 10);
+ console.log(10 < 10);
+ console.log(123 === '123');
+console.log(123 !== '123' );
+

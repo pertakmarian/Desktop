b6808d72df7149ab432feeef2f23e67f9d0b1352
mac <mpertak91@gmail.com>
2022-02-14 19:38:24 +0200

    home3

---
+++ JS/homework 5/homejs.js
@@ -0,0 +1,49 @@
+// - створити функцію яка обчислює та повертає площу прямокутника
+//const res = (a,b)=>{
+   // return a*b;
+//}
+//console.log(res(20,20))
+// - створити функцію яка обчислює та повертає площу круга
+//const  colo =(r)=> {
+   // let radius = r ** 2
+    //return 3.14 * radius
+//}
+//console.log(colo(20))[
+//- створити функцію яка приймає масив та виводить кожен його елемент
+//let mas =[12,45,65,'car','dog','cat']
+//const  bigmas =(mas)=>{
+  //  for (let i=0; i<mas.length; i++){
+      //  document.write(mas[i])
+   // }
+
+//}
+//bigmas(mas)
+//  - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+//const ulres = (res)=>{
+   // document.write('<ul>')
+   // document.write('<li> ${res} </li>')
+   // document.write('<li> ${res} </li>')
+   // document.write('<li> ${res} </li>')
+    //document.write('</ul>')
+
+//}
+//ulres()
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+//const  element =(txt,number)=>{
+  //  document.write('<ul>')
+    //for (let i=0; i<number;i++){
+       // document.write(`<li>${txt}</li>`)
+       // document.write('</ul>')
+   // }
+//}
+//element('hello',3)
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+//let mas =[22,34,'car',true]
+//const element =(bigmas)=> {
+   // document.write(`<ul>`)
+   // for ( let i=0;i<bigmas.length;i++){
+       // document.write(`<li>${bigmas[i]}</li>`)
+   // }
+  // document.write(`</ul>`)
+//}
+//element(mas)

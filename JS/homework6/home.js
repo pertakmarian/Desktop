f32c65b2aff17e47d506406226c8c09ed0ee789c
mac <mpertak91@gmail.com>
2022-02-17 17:35:56 +0200

    home 6

---
+++ JS/homework6/home.js
@@ -0,0 +1,62 @@
+//- Знайти та вивести довижину настипних стрінгових значень
+//let mas =['hello world', 'lorem ipsum', 'javascript is cool']
+
+//let length1='hello world';
+//console.log(lenght2.length);
+
+//let lenght2='lorem ipsum';
+//console.log(lenght2.length);
+
+ //let lenght3='javascript is cool';
+//console.log(lenght3.length)
+//Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+//let length1='hello world';
+//let one =length1.toUpperCase()
+//console.log(one)
+
+//let lenght2='lorem ipsum';
+//let two =lenght2.toUpperCase()
+//console.log(two)
+
+//let lenght3='javascript is cool';
+//let three= lenght3.toUpperCase()
+//console.log(three)
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+//let length1='HELLO WORLD';
+//let one =length1.toLowerCase()
+//console.log(one)
+
+//let lenght2='LOREM IPSUM';
+//let two =lenght2.toLowerCase()
+//console.log(two)
+
+//let lenght3='JAVASCRIPT IS COOL';
+//let three= lenght3.toLowerCase()
+//console.log(three)
+//Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+//let str = ' dirty string   '
+//let strings=str.trim()
+//console.log(strings)
+
+//Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//let stringToarray = (str) => {
+  // return str.split(' ')
+ //}
+ //let str = 'Каждый охотник желает знать'
+//let arr = stringToarray(str)
+ //console.log(arr)
+ //- /Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
+    //let str = 'Каждый охотник желает знать';
+//document.writeln(delete_characters(str, 7)); // Каждый
+//let delete_characters=(str, length)
+//let delete_characters = (str, index) => {
+  //   return str.substr(0, index);
+ //};
+// let str = 'Каждый охотник желает знать';
+// console.log(delete_characters(str,4))
+//Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
+    //let str = "HTML JavaScript PHP";
+//document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
+
